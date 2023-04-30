dbb32906b7becc92fe51e799af781a3b251bb341
Anton Yurchenko <anton.yurchenko@valtech.com>
2023-04-30 13:54:08 +0300

    task 2 done

---
+++ task1/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Diagnostics;
+
+namespace OSLab6
+{
+  internal class Program
+  {
+    static int size = 1000;
+    static int[,] matrix1 = new int[size, 1000];
+    static int[,] matrix2 = new int[1000, size];
+    static void Main()
+    {
+      initMatrix(ref matrix1);
+      initMatrix(ref matrix2);
+
+      if (matrix1.GetLength(0) != matrix2.GetLength(1)) throw new ArgumentException();
+
+      var stopwatch = new Stopwatch();
+      stopwatch.Start();
+      var res1 = multiplyMatrice(matrix1, matrix2);
+      stopwatch.Stop();
+      Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+      stopwatch.Reset();
+      stopwatch.Start();
+      var res2 = multiplyMatriceThread(matrix1, matrix2);
+      stopwatch.Stop();
+      Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+      for (int i = 0; i < matrix1.GetLength(0); i++)
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+          if (res1[i, j] != res2[i, j]) throw new Exception();
+    }
+
+    private static int[,] multiplyMatrice(int[,] matrix1, int[,] matrix2)
+    {
+      var matrix = new int[matrix1.GetLength(0), matrix1.GetLength(0)];
+
+      for (int i = 0; i < matrix.GetLength(0); i++)
+      {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+          var row = GetRow(matrix1, i);
+          var col = GetColumn(matrix2, j);
+          matrix[i, j] = multiplyVectors(row, col);
+        }
+      }
+      return matrix;
+    }
+
+    private static int[,] multiplyMatriceThread(int[,] matrix1, int[,] matrix2)
+    {
+      var matrix = new int[matrix1.GetLength(0), matrix1.GetLength(0)];
+      var tasks = new Task[matrix.GetLength(0)];
+
+      for (int i = 0; i < matrix.GetLength(0); i++)
+      {
+        var counter = i;
+        var length = matrix.GetLength(0);
+        var task = new Task(() =>
+        {
+          for (int j = 0; j < length; j++)
+          {
+            var row = GetRow(matrix1, counter);
+            var col = GetColumn(matrix2, j);
+            matrix[counter, j] = multiplyVectors(row, col);
+          }
+        });
+        tasks[i] = task;
+        task.Start();
+      }
+      Task.WaitAll(tasks);
+      return matrix;
+    }
+
+    private static void initMatrix(ref int[,] matrix)
+    {
+      var random = new Random();
+      for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+          matrix[i, j] = i * matrix.GetLength(1) + j;
+        }
+    }
+
+    private static int multiplyVectors(int[] vector1, int[] vector2)
+    {
+      if (vector1.Length != vector2.Length) throw new ArgumentException();
+      var result = 0;
+      for (int i = 0; i < vector1.Length; i++)
+      {
+        result += vector1[i] * vector2[i];
+      }
+      //Console.WriteLine(Task.CurrentId);
+      //Console.WriteLine(result);
+      return result;
+    }
+    private static int[] GetColumn(int[,] matrix, int columnNumber)
+    {
+      return Enumerable.Range(0, matrix.GetLength(0))
+              .Select(x => matrix[x, columnNumber])
+              .ToArray();
+    }
+
+    private static int[] GetRow(int[,] matrix, int rowNumber)
+    {
+      return Enumerable.Range(0, matrix.GetLength(1))
+              .Select(x => matrix[rowNumber, x])
+              .ToArray();
+    }
+  }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+
+namespace task2
+{
+  internal class Program
+  {
+    static object padlock = new object();
+    static ulong a = 0;
+
+    static void Main(string[] args)
+    {
+
+      var stopwatch = new Stopwatch();
+      stopwatch.Start();
+
+      var task1 = new Task(changeValueLock);
+      var task2 = new Task(changeValueLock);
+      task1.Start();
+      task2.Start();
+      Task.WaitAll(task1, task2);
+
+      stopwatch.Stop();
+      Console.WriteLine(a);
+      Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+    }
+
+    private static void changeValueLock()
+    {
+      for (int i = 0; i < 1_000_000_000; i++)
+      {
+        lock (padlock)
+        {
+          a++;
+        }
+      }
+    }
+
+    private static void changeValueSync()
+    {
+      for (int i = 0; i < 1000; i++)
+      {
+        var b = a;
+        Thread.Sleep(1);
+        a =b + 1;
+      }
+
+    }
+
+    private static void changeValue()
+    {
+      for (int i = 0; i < 1_000_000; i++) a++;
+    }
+
+  }
+}
